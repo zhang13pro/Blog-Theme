ac7ac15ee01723d8800ac754d926167a99544e86
Lex（章波） <1796021400@qq.com>
2021-05-22 12:58:51 +0800

    Initial commit

---
+++ src/.vuepress/README.md
@@ -0,0 +1,4 @@
+## What
+So,this is a theme for blog with Vuepress.
+
+Let's do it.
---
+++ src/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img
+      src="https://cdn.jsdelivr.net/gh/zhang13pro/blogImg/img/404.jpg"
+      alt=""
+    />
+    <p class="number">404</p>
+    <p class="content">
+      Hm,the page you were looking for doesn't seem to exist anymore:(
+    </p>
+    <button>get back</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+img {
+  height: 100vh;
+  width: 100vw;
+  object-fit: cover;
+}
+
+.number {
+  color: green;
+}
+
+.content {
+  color: red;
+  font-family: Times New Roman;
+  font-size: 22px;
+}
+</style>
